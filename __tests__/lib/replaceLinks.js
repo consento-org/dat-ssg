d5c5e839504f78316e0bbbc07c91b28440bd5597
Martin Heidegger <martin.heidegger@gmail.com>
2020-03-12 04:59:59 +0900

    Adding support for automatically downloading og|twitter:images
    Signed-off-by: Martin Heidegger <martin.heidegger@gmail.com>

--- __tests__/lib/replaceLinks.js
+++ __tests__/lib/replaceLinks.js
@@ -5,12 +5,17 @@ describe('replaceOptions', () => {
   it('testing with sample data', async () => {
     const prefix = `${__dirname}/_data/tanja`
     const filepath = `${prefix}.html`
-    const { value: patternMap } = replaceOptions('write.georepublic.net', 'https://consento.org').values().next()
+    const images = []
+    const { value: patternMap } = replaceOptions('write.georepublic.net', 'https://consento.org', image => images.push(image)).values().next()
     expect(await iterToString(
       replace({
         stream: readStringFile(filepath),
         patternMap
       })
     )).toBe(await readFile(`${prefix}.expected.html`, 'utf-8'))
+    expect(images).toEqual([
+      'https://write.georepublic.net/content/images/2020/03/human-centric@1x-1.png',
+      'https://write.georepublic.net/images/2020/03/human-centric@1x.png'
+    ])
   })
 })
